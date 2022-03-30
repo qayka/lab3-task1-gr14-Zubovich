73b84261ee38cd991b7a6079923e291b137ad569
Александр Зубович <49609708+qayka@users.noreply.github.com>
2022-03-30 15:45:52 +0000

    Created tests in main file

--- src/main.c
+++ src/main.c
@@ -1,8 +1,22 @@
-/* hello.c */
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-void main (void)
+int main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  char* ptr;
+  int arr_size = 50;
+  int* arr = malloc(arr_size * sizeof(int));
+  srand(time(NULL));
+
+  for(int i =0;i<arr_size;i++)
+    arr[i] = rand()%1000;
+
+  printf("Random array:\n");
+
+  for(int i =0;i<arr_size;i++)
+    printf("arrd[%d] = %d\n",i,arr[i]);
+
+  free(arr);
+  return 0;
 }
