2814773dc098cb62a86c46615f26e8139a52de65
Александр Зубович <49609708+qayka@users.noreply.github.com>
2022-05-02 17:25:58 +0000

    main.c Fix

--- src/main.c
+++ src/main.c
@@ -2,21 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main (void)
+
+
+int Check(int number) {
+  while(number > 1 && (number % 5) == 0){
+    number = number/5;
+    }
+  if(number == 1)
+    return 1;
+  else 
+    return 0;
+}
+
+
+int main (int argc,char** argv)
 {
+  if(argc<2){
+      printf("Invalid number of arguments");
+      return 1;
+  }
   char* ptr;
-  int arr_size = 50;
+  int arr_size = strtol(argv[1],&ptr,10);
   int* arr = malloc(arr_size * sizeof(int));
   srand(time(NULL));
 
   for(int i =0;i<arr_size;i++)
     arr[i] = rand()%1000;
+  arr[2] = 125;
 
-  printf("Random array:\n");
-
-  for(int i =0;i<arr_size;i++)
-    printf("arrd[%d] = %d\n",i,arr[i]);
-
+  for(int i = 0;i<arr_size;i++)
+    if(Check(arr[i]))
+      printf("arrd[%d] = %d\n",i,arr[i]);
+      
   free(arr);
   return 0;
 }
